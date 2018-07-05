a280e86bc3bf2a3ea565db3f7763c17b4ac1ef89
may <mehrabhoque@gmail.com>
2018-07-05 15:50:27 -0400

    Refactor conf_schema.py into Config and TodoConfig classes.

--- todo/conf_schema.py
+++ todo/conf_schema.py
@@ -1,20 +1,48 @@
-conf_filename = '.todo.json'
+import os
+import json
 
-required_fields = [
-    "secret"
-]
+class Config:
+    def __init__(self, conf_json, required, optional):
+        self.required_fields = required
+        self.optional_fields = optional
+        self.conf_json = self.validate_conf(conf_json)
 
-optional_fields = {
-    "show_inbox" : True
-}
+    def validate_conf(self, conf_json):
+        for required in self.required_fields:
+            if required not in conf_json:
+                raise ValueError(".todo.conf is missing {}".format(required))
 
-def validate_conf(conf_json):
-    for required in required_fields:
-        if required not in conf_json:
-            raise ValueError(".todo.conf is missing {}".format(required))
+        for optional in self.optional_fields:
+            if optional not in conf_json:
+                conf_json[optional] = self.optional_fields[optional]
 
-    for optional in optional_fields:
-        if optional not in conf_json:
-            conf_json[optional] = optional_fields[optional]
+        return conf_json
 
-    return conf_json
+    def __getitem__(self, key):
+        return self.conf_json[key]
+
+    def __setitem__(self, key, value):
+        self.conf_json[key] = value
+
+class TodoConfig(Config):
+    def __init__(self, conf_json, required, optional):
+        super().__init__(conf_json, required, optional)
+
+    @staticmethod
+    def get_config():
+        conf_filename = '.todo.json'
+
+        required_fields = [
+            "secret"
+        ]
+
+        # Optional fields paired with their default value.
+        optional_fields = {
+            "show_inbox" : True,
+            "default_project" : None
+        }
+
+        conf_filepath = os.path.join(os.path.expanduser('~'), conf_filename)
+        with open(conf_filepath) as conf_file:
+            conf_json = json.load(conf_file)
+            return TodoConfig(conf_json, required_fields, optional_fields)
--- todo/todo.py
+++ todo/todo.py
@@ -1,10 +1,8 @@
-import json
-import os
 import colored
 
 from cli import TodoistCLI
 import wrapper
-from conf_schema import conf_filename, validate_conf
+from conf_schema import TodoConfig
 from color import prnt_str, PURPLE, ORANGE, BLUE
 
 def init_cli(conf):
@@ -15,12 +13,9 @@ def init_cli(conf):
     todoist_cli.cmdloop(prnt_str('todoist', PURPLE))
 
 def main():
-    conf_filepath = os.path.join(os.path.expanduser('~'), conf_filename)
-    with open(conf_filepath) as conf_file:
-        conf_json = json.load(conf_file)
-        conf = validate_conf(conf_json)
-        wrapper.init(conf)
+    conf = TodoConfig.get_config()
 
+    wrapper.init(conf)
     init_cli(conf)
 
 if __name__ == '__main__':
