da7bcd4f6b7e4180199976a546dd412c904b3602
may <mehrabhoque@gmail.com>
2018-08-26 17:33:08 -0400

    Break up inject() into several helpers
    The inject() function was way too big and nasty. It has been broken down
    into the constituent functions:
    
      1. _get_pat_and_hint(arg) => (pat, hint)
      2. _inject_id(self, args, i, pat, hint) => None
    
    This should make things a lot more readable.
    
    Making those nested functions was explored, but the readability is
    horrible.

--- todo/cli_helpers.py
+++ todo/cli_helpers.py
@@ -65,38 +65,47 @@ class arglen:
         return arglen_check
 
 
-def inject(func):
+def _get_pat_and_hint(arg):
     inject_base_pat = '%{}:["]?({})["]?'
     hint_base = '%{}'
 
+    if '%p:' in arg:
+        pat = inject_base_pat.format('p', '\d+')
+        hint = hint_base.format('p')
+    elif '%s:' in arg:
+        pat = inject_base_pat.format('s', '.+')
+        hint = hint_base.format('s')
+    elif '%:' in arg:
+        pat = inject_base_pat.format('', '.+')
+        hint = hint_base.format('')
+    elif '%c:' in arg:
+        pat = inject_base_pat.format('c', '')
+        hint = hint_base.format('c')
+
+    return pat, hint
+
+
+def _inject_id(self, args, i, pat, hint):
+    res = re.compile(pat).search(args[i])
+    if not res:
+        raise CmdError("Invalid substitution argument")
+    val = res.group(1)
+    inject_id = str(self.state.fetch(val, hint=hint).obj_id)
+    args[i] = re.sub(pat, inject_id, args[i])
+
+
+def inject(func):
     @wraps(func)
     def inject_arg(self, args):
         for i in range(len(args)):
             arg = args[i]
-            if '%p:' in arg:
-                pat = inject_base_pat.format('p', '\d+')
-                hint = hint_base.format('p')
-            elif '%s:' in arg:
-                pat = inject_base_pat.format('s', '.+')
-                hint = hint_base.format('s')
-            elif '%:' in arg:
-                pat = inject_base_pat.format('', '.+')
-                hint = hint_base.format('')
-            elif '%c:' in arg:
-                pat = inject_base_pat.format('c', '')
-                hint = hint_base.format('c')
-            else:
+            pat, hint = _get_pat_and_hint(arg)
+            if not pat and not hint:
                 continue
 
-            res = re.compile(pat).search(arg)
-            if not res:
-                raise CmdError("Invalid substitution argument")
-            val = res.group(1)
-            inject_id = str(self.state.fetch(val, hint=hint).obj_id)
-            args[i] = re.sub(pat, inject_id, arg)
+            _inject_id(self, args, i, pat, hint)
 
         func(self, args)
-        # readline_inject(args)
 
     return inject_arg
 
