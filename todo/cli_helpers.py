ad19ffcf0b854e1b38d9c32060927d513e62eaf2
may <mehrabhoque@gmail.com>
2018-11-14 23:49:39 -0500

    Make pattern/hint regex more robust
    This leaves the grouping decision to each pattern instead of hardcoding
    the grouping for every pattern. This lets something like %s:"" mandate
    quotes.

--- todo/cli_helpers.py
+++ todo/cli_helpers.py
@@ -96,23 +96,23 @@ class arglen:
 
 
 def _get_pat_and_hint(arg):
-    inject_base_pat = '%{}:["]?({})["]?'
+    inject_base_pat = '%{}:["]?{}["]?'
     hint_base = '%{}'
 
     if '%p:' in arg:
-        pat = inject_base_pat.format('p', '\d+')
+        pat = inject_base_pat.format('p', '(\d+)')
         hint = hint_base.format('p')
     elif '%s:' in arg:
-        pat = inject_base_pat.format('s', '.+')
+        pat = inject_base_pat.format('s', '"(.+)"')
         hint = hint_base.format('s')
     elif '%:' in arg:
-        pat = inject_base_pat.format('', '.+')
+        pat = inject_base_pat.format('', '(.+)')
         hint = hint_base.format('')
     elif '%c:' in arg:
-        pat = inject_base_pat.format('c', '')
+        pat = inject_base_pat.format('c', '()')
         hint = hint_base.format('c')
     elif '%cp:' in arg:
-        pat = inject_base_pat.format('cp', '\d+')
+        pat = inject_base_pat.format('cp', '(\d+)')
         hint = hint_base.format('cp')
     else:
         pat, hint = None, None
